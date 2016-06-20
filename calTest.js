45c18c6cd8c6665549dba66d203dd338209ab32f
Hugh <hugh.oneill.4@gmail.com>
2016-06-20 10:03:50 -0600

    Successfully populated event well with title of workout and list of workout items.

--- calTest.js
+++ calTest.js
@@ -7,19 +7,21 @@ $(document).ready(function() {
     clickDate(moment())
     // -----------------Workout Objects-------------------------
     // -----------------A1 Workout Objs-------------------------
-    let ubfA1 = new Workout('ubfA1', 'Upper Body Flexibility', 'https://www.youtube.com/watch?v=Ozd_56IHdfM', 'flex')
-    let lbsA1 = new Workout('lbsA1', 'Lower Body Strength', 'https://www.youtube.com/watch?v=MYNWjf0qjxE', 'lift')
-    let restA1 = new Workout('restA1', 'Rest Day', 'https://www.youtube.com/watch?v=qrx1vyvtRLY', 'rest')
-    let cardioA1 = new Workout('cardioA1', 'Cardio', 'https://www.youtube.com/watch?v=iTLtv0hoSHU', 'cardio')
+    let ubfA1 = new Workout('ubfA1', 'Upper Flex', 'https://www.youtube.com/watch?v=Ozd_56IHdfM', 'flex', 'Upper Body Flexibility A1', ['Lower Back Mobility','Biceps Stretch', 'Pec Stretch'])
+    let lbsA1 = new Workout('lbsA1', 'Lower Strength', 'https://www.youtube.com/watch?v=MYNWjf0qjxE', 'lift', 'Lower Body Strength A1', ['body weight squats', 'alternating lunges', 'swiss ball hamstring pulls'])
+    let restA1 = new Workout('restA1', 'Rest Day', 'https://www.youtube.com/watch?v=qrx1vyvtRLY', 'rest', 'Rest Day', ['Enjoy your rest day'])
+    let cardioA1 = new Workout('cardioA1', 'Cardio', 'https://www.youtube.com/watch?v=iTLtv0hoSHU', 'cardio', 'Cardio Workout', ['biking', 'jogging', 'swimming'])
     //-----------------Workout Array Input----------------------
     let workoutA1 = [ubfA1,lbsA1,restA1]
     let workoutA2 = [ubfA1,lbsA1,restA1,cardioA1]
     //--------------------Workout Constructor-------------------
-    function Workout(id, title, url, className)     {
+    function Workout(id, title, url, className, descrip, list)     {
         this.id = id
         this.title = title
         this.url = url
         this.className = className
+        this.descrip = descrip
+        this.list = list
         this.BGColor = getBG(className)
         this.txtColor = getTxt(className)
 
@@ -77,7 +79,10 @@ $(document).ready(function() {
                 start:dateArr[i],
                 url:workoutArr[i].url,
                 backgroundColor:workoutArr[i].BGColor,
-                textColor: workoutArr[i].txtColor
+                textColor: workoutArr[i].txtColor,
+                descrip: workoutArr[i].descrip,
+                list:workoutArr[i].list
+
             })
         }
         return scheduleArr
@@ -89,6 +94,13 @@ $(document).ready(function() {
         weekends: true,
         dayClick: function(date) {
             clickDate(date)
+            var dayEvents = $('#calendar').fullCalendar('clientEvents', function(event) {
+                return moment(event.start).isSame(date)
+                
+            })
+            console.log(dayEvents[0])
+            pushWorkout(dayEvents)
+            
             	
         },
         allDayDefault: true,
@@ -128,10 +140,11 @@ $(document).ready(function() {
     function addDays(date,length){
         return moment(date).add(length,'days')
     }
-    
+    // click function for calendar well dates 
     function clickDate(date) {
         let newDate = moment(date,'YYYY-MM-DD').format('MMMM D')
         $('h2.dateTitle').html(newDate)
+       
         
     }
     // function findTitle(title) {
@@ -139,15 +152,31 @@ $(document).ready(function() {
     // }
     // console.log(programA1.eventPush.find(findTitle))
     let compareDate = '2016-06-30'
+    // compare date function for find
     function findDate(clickDate) {
         
         let clickFormat = moment(clickDate.start._d).format('YYYY-MM-DD')
         return clickFormat === compareDate
     }
-    
+    // get workout title from object with the findDate function based on date chosen
     function getCurrentW() {
-        console.log(programA1.eventPush.find(findDate).title)
+        let clientEventsArr = $('#calendar').fullCalendar('clientEvents')
+        console.log(clientEventsArr.find(findDate).title)
         return(programA1.eventPush.find(findDate)).title
     }
     getCurrentW()
+    function pushWorkout(dayEvents) {
+            $('h3.workoutTitle').html(dayEvents[0].descrip)
+            $('#workoutList').empty()
+            for (let i=0; i < dayEvents[0].list.length; i++) {
+                $('#workoutList').append(
+                $('<a/>')
+                    .addClass('list-group-item')
+                    .prop('href', '#')
+                    .html('<div class=\'c1\'><h4>' + dayEvents[0].list[i] + '</h4>')
+                    
+                )
+            }
+            
+        }
 }); 
